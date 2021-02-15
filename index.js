28c08939899af3f8061451e2498a3a4013511441
Dev Frias <ddfrias14@gmail.com>
2021-02-15 09:40:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase())
 }
-function logWhispher(string) {
+function logWhisper(string) {
   console.log(string.toLowerCase())
 }
