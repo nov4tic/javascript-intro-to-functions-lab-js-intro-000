fcc7c254eddc4dcbd35cb38a5a94e7a7c5a905df
Dev Frias <ddfrias14@gmail.com>
2021-02-15 09:52:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function logWhisper(string) {
 var uppercase = 'STRING'
 var lowercase = 'string'
 function sayHiToGrandma(string) {
-  if ('string' === lowercase) {
+  if ('string' === uppercase) {
+    return "YES INDEED!" }
+  else if ('string' === lowercase) {
     return "I can't hear you!" }
-  else if ('string' === uppercase) {
-    return "YES INDEED" }
   else if ('string' === "I love you, Grandma.") {
     return "I love you, too"
 }}
